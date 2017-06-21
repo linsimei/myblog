a9301000dbc4ac1bc94720a8b24adf26af278214
linsimei <772094242@qq.com>
2017-06-21 21:42:32 +0800

    2017/06/21 21：42

--- public/javascripts/register.js
+++
@@ -1,6 +0,0 @@
-var status = document.getElementsByClassName('status')[0].getElementsByTagName('a');
-// for (var i = 0; i < status.length; i++) {
-//     status[i].onclick = function() {
-//         status[i].className = "";
-//     }
-//     this.className = "on";}
